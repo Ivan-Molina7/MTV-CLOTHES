362b81be3489dacb01a8ee212a20c742045d5a72
Ivan Molina <ivanadrian951@gmail.com>
2022-04-29 20:32:46 -0300

    html y css terminados

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const nav__list = document.querySelector(".nav__list");
+
+
+hamburger.addEventListener("click",() =>{
+    hamburger.classList.toggle("active");
+    nav__list.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav__list__item__a").forEach(n => n.
+    addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav__list.classList.remove("active");
+}))
+
