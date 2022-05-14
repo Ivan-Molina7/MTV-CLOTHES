e18d1d0b07138255f37c87c49059c9fa05b336d5
Ivan Molina <ivanadrian951@gmail.com>
2022-05-14 01:14:19 -0300

    Pagina final 1.0

--- js/main.js
+++ js/main.js
@@ -13,3 +13,51 @@ document.querySelectorAll(".nav__list__item__a").forEach(n => n.
     nav__list.classList.remove("active");
 }))
 
+
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const seconds = document.getElementById("seconds");
+
+const newYear = '31 may 2022';
+
+function countTimer(){
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const daysCalc = Math.floor(totalSeconds / 3600 / 24);
+    const hoursCalc = Math.floor(totalSeconds / 3600) % 24;
+    const minsCalc = Math.floor(totalSeconds / 60) % 60;
+    const secondsCalc = Math.floor(totalSeconds % 60);
+    
+    days.innerHTML = daysCalc;
+    hours.innerHTML = hoursCalc;
+    mins.innerHTML = minsCalc;
+    seconds.innerHTML = secondsCalc;
+}
+countTimer();
+
+setInterval(countTimer, 1000);
+
+
+var header = document.querySelector('header'),
+    inicio = document.querySelector('.container__inicio'),
+    alturaInicio = parseFloat(getComputedStyle(inicio).height) 
+
+
+
+    
+
+window.addEventListener('scroll', e=>{
+
+     if(window.scrollY > alturaInicio - 80){
+        header.classList.add("change")
+    }
+    else {
+        header.classList.remove("change")
+     }
+     
+   
+})
